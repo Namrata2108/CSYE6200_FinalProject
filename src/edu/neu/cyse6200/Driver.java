37913d6293792f15a9524907d838e8224570719a
Ebenezer Ajay Williams <ebby.williams1996@gmail.com>
2021-12-09 11:57:46 -0500

    start with this

--- src/edu/neu/cyse6200/Driver.java
+++ src/edu/neu/cyse6200/Driver.java
@@ -6,8 +6,9 @@ import java.awt.EventQueue;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
+import javax.swing.JLabel;
 
-public class Application extends JFrame {
+public class Driver extends JFrame {
 
 	private JPanel contentPane;
 
@@ -18,7 +19,7 @@ public class Application extends JFrame {
 		EventQueue.invokeLater(new Runnable() {
 			public void run() {
 				try {
-					Application frame = new Application();
+					Driver frame = new Driver();
 					frame.setVisible(true);
 				} catch (Exception e) {
 					e.printStackTrace();
@@ -30,13 +31,16 @@ public class Application extends JFrame {
 	/**
 	 * Create the frame.
 	 */
-	public Application() {
+	public Driver() {
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 450, 300);
+		setBounds(100, 100, 776, 389);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 		contentPane.setLayout(new BorderLayout(0, 0));
 		setContentPane(contentPane);
+		
+		JLabel lblDaycareManagementSystem = new JLabel("DayCare Management System");
+		contentPane.add(lblDaycareManagementSystem, BorderLayout.NORTH);
 	}
 
 }
